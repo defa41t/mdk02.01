8697652ad9978312f197d11e180812bb8894ca40
Tabakov Ivan <ivan.tabakov.10@gmail.com>
2025-11-04 20:31:57 +0500

    Добавлена библиотека TestLibrary с классом TestClass

---
+++ TestSolution/TestLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace TestLibrary
+{
+    public class TestClass
+    {
+        public int x;
+        public int y;
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"x = {x}, y = {y}");
+        }
+
+        public double Power()
+        {
+            return Math.Pow(x, y);
+        }
+    }
+}
